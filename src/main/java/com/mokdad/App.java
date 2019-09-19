0d9bbe1d5398f3b699871e6ed05d1d0b01b8cda9
Mokdad Salhi <ms@rr.com>
2019-09-19 11:14:51 +0100

    "commit ini1"

---
+++ src/main/java/com/mokdad/App.java
@@ -0,0 +1,13 @@
+package com.mokdad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello My friend!" );
+    }
+}
