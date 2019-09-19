40f8f6e3a89a2ef5ac2b3c256dd30c3b2ae0dcc3
mokdad1 <55434318+mokdad1@users.noreply.github.com>
2019-09-19 17:33:46 +0200

    Update App.java

--- src/main/java/com/mokdad/App.java
+++ src/main/java/com/mokdad/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello My friend! hi" );
+        System.out.println( "Hello My friend! hi guys" );
     }
 }
